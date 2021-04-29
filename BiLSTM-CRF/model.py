578bac2cdb44819e59118f47fe26e6ed5c93509d
WhiteGive-Boy <56426042+WhiteGive-Boy@users.noreply.github.com>
2021-04-29 10:21:56 +0800

    Add files via upload
    add cuda

--- BiLSTM-CRF/model.py
+++ BiLSTM-CRF/model.py
@@ -6,19 +6,20 @@ import torch.optim as optim
 torch.manual_seed(1)
 START_TAG = "<START>"
 STOP_TAG = "<STOP>"
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 def argmax(vec):
-    '''
-    计算一维vec最大值的坐标
-    '''
     # return the argmax as a python int
     _, idx = torch.max(vec, 1)
     return idx.item()
 
 
+def prepare_sequence(seq, to_ix):
+    idxs = [to_ix[w] for w in seq]
+    return torch.tensor(idxs, dtype=torch.long).to(device)
+
+
+# Compute log sum exp in a numerically stable way for the forward algorithm
 def log_sum_exp(vec):
-    '''
-    计算vec的 log(sum(exp(xi)))=a+log(sum(exp(xi-a)))
-    '''
     max_score = vec[0, argmax(vec)]
     max_score_broadcast = max_score.view(1, -1).expand(1, vec.size()[1])
     return max_score + \
@@ -42,7 +43,7 @@ class Model(nn.Module):
         self.hidden2tag = nn.Linear(hidden_dim, self.tagset_size)
 
         # Matrix of transition parameters.  Entry i,j is the score of
-        # transitioning *to* i *from* j = trans[i][j]
+        # transitioning *to* i *from* j.
         self.transitions = nn.Parameter(
             torch.randn(self.tagset_size, self.tagset_size))
 
@@ -54,19 +55,14 @@ class Model(nn.Module):
         self.hidden = self.init_hidden()
 
     def init_hidden(self):
-        return (torch.randn(2, 1, self.hidden_dim // 2),
-                torch.randn(2, 1, self.hidden_dim // 2))
+        return (torch.randn(2, 1, self.hidden_dim // 2).to(device),
+                torch.randn(2, 1, self.hidden_dim // 2).to(device))
 
     def _forward_alg(self, feats):
-        '''
-        :param feats: LSTM+hidden2tag的输出
-        :return: 所有tag路径的score和
-        forward_var：之前词的score和
-        '''
         # Do the forward algorithm to compute the partition function
-        init_alphas = torch.full((1, self.tagset_size), -10000.)
+        init_alphas = torch.full((1, self.tagset_size), -10000.).to(device)
         # START_TAG has all of the score.
-        init_alphas[0][self.tag2id[START_TAG]] = 0. #
+        init_alphas[0][self.tag2id[START_TAG]] = 0.
 
         # Wrap in a variable so that we will get automatic backprop
         forward_var = init_alphas
@@ -88,7 +84,7 @@ class Model(nn.Module):
                 # The forward variable for this tag is log-sum-exp of all the
                 # scores.
                 alphas_t.append(log_sum_exp(next_tag_var).view(1))
-            forward_var = torch.cat(alphas_t).view(1, -1)
+            forward_var = torch.cat(alphas_t).view(1, -1).to(device)
         terminal_var = forward_var + self.transitions[self.tag2id[STOP_TAG]]
         alpha = log_sum_exp(terminal_var)
         return alpha
@@ -102,9 +98,9 @@ class Model(nn.Module):
         return lstm_feats
 
     def _score_sentence(self, feats, tags):
-        # Gives the score of a provided tag sequence 当前句子的tag路径score
-        score = torch.zeros(1)
-        tags = torch.cat([torch.tensor([self.tag2id[START_TAG]], dtype=torch.long), tags])
+        # Gives the score of a provided tag sequence
+        score = torch.zeros(1).to(device)
+        tags = torch.cat([torch.tensor([self.tag2id[START_TAG]], dtype=torch.long).to(device), tags]).to(device)
         for i, feat in enumerate(feats):
             score = score + \
                 self.transitions[tags[i + 1], tags[i]] + feat[tags[i + 1]]
@@ -112,19 +108,19 @@ class Model(nn.Module):
         return score
 
     def _viterbi_decode(self, feats):
-        backpointers = [] #路径保存
+        backpointers = []
 
         # Initialize the viterbi variables in log space
-        init_vvars = torch.full((1, self.tagset_size), -10000.)
+        init_vvars = torch.full((1, self.tagset_size), -10000.).to(device)
         init_vvars[0][self.tag2id[START_TAG]] = 0
 
         # forward_var at step i holds the viterbi variables for step i-1
         forward_var = init_vvars
-        for feat in feats: # for every word
+        for feat in feats:
             bptrs_t = []  # holds the backpointers for this step
             viterbivars_t = []  # holds the viterbi variables for this step
 
-            for next_tag in range(self.tagset_size): #for every possible tag of the word
+            for next_tag in range(self.tagset_size):
                 # next_tag_var[i] holds the viterbi variable for tag i at the
                 # previous step, plus the score of transitioning
                 # from tag i to next_tag.
@@ -136,7 +132,7 @@ class Model(nn.Module):
                 viterbivars_t.append(next_tag_var[0][best_tag_id].view(1))
             # Now add in the emission scores, and assign forward_var to the set
             # of viterbi variables we just computed
-            forward_var = (torch.cat(viterbivars_t) + feat).view(1, -1)
+            forward_var = (torch.cat(viterbivars_t) + feat).to(device).view(1, -1)
             backpointers.append(bptrs_t)
 
         # Transition to STOP_TAG
